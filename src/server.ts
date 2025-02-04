ffd46fa929cdad6ea59c4a60528f0075c9e4c290
zachuri <punsalang.zachary@gmail.com>
2025-02-04 00:13:14 -0800

    chore: updated to bun

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import { serve } from "@hono/node-server";
-
 import { showRoutes } from "hono/dev";
 import app from "./app";
 
 const port = 8787;
 console.log(`Server is running on port - ${port}`);
 
-serve({
+Bun.serve({
 	fetch: app.fetch,
 	port,
 });
